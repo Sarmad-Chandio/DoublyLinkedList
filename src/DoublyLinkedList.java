e01857872fa2d1a624036b69d591c19f0fcdfcb1
sarmad.sikandar <sarmad.sikandar@systemsltd.com>
2024-03-16 01:33:11 +0500

    DoubleLinked list : remove at, set at,  insert at, get by index methods

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -82,13 +82,101 @@ public class DoublyLinkedList {
             head.prev=null;
 
         }
-
-
         length--;
+        return temp;
+    }
+
+    public Node get(int index){
+
+        if (index<0 || index >length){
+            return null;
+        }
+        if (index < length/2){
+            Node temp=head;
+            //start from head
+            for (int i=0; i<index; i++){
+                temp=temp.next;
+            }
+            return temp;
+        }else{
+            //Node temp=head;
+            //start from tail
+            Node temp=tail;
+            for (int i=index; i>=index; i--){
+                temp=temp.prev;
+            }
+            return temp;
+        }
+    }
+
+    public boolean set(int index, int value){
+        Node temp=get(index);
+        if (temp==null){
+            return false;
+        }
+
+        temp.value=value;
+
+        return true;
+    }
+
+    public boolean insert (int index, int value){
+        //insertion for empty list (it will be handled by insertion by first node, because we are going to use append method)
+        //insertion at first node
+        //insertion at last index
+        //insertion at any index
+        //edge case : index should not be negative are greater than length of the list
+        if(index < 0 || index > length) return false;
+
+        if (index==0){
+            prepend(value);
+            return true;
+        }
+        if(index==length){
+            append(value);
+            return true;
+        }
+            Node newNode= new Node(value);
+            Node temp=get(index);
+            if (temp==null){return false;}
+            Node beforeTemp=temp.prev;
+            beforeTemp.next=newNode;
+            temp.prev=newNode;
+            newNode.prev=beforeTemp;
+            newNode.next=temp;
+            length++;
+            return true;
+
+    }
+
+    public Node remove(int index){
+        //remove from first node
+        //remove from last node
+        //remove from end of the node
+        //edge case: index should not be greater than size of list or less than zero
+        if (index < 0 || index >= length) return null;
+        if (index==0){
+            return removeFirst();
+        }
+        if (index==length-1){
+            return removeLast();
+        }
+        Node temp=get(index);
+        Node after=temp.next;
+        Node before=temp.prev;
+        temp.prev=null;
+        temp.next=null;
+        after.prev=before;
+        before.next=after;
+        length--;
+
 
         return temp;
 
     }
+
+
+
 
     public Node getHead() {
         return head;
--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,22 @@ public class Main {
         dll.append(1);
         dll.append(8);
         dll.append(2);
+        dll.append(9);
+        dll.append(3);
+        dll.append(10);
 
         dll.printList();
+
+        System.out.println("******* Remove index");
+        System.out.println("removed="+dll.remove(17));
+        dll.printList();
+
+        System.out.println("*******");
+
+
+        System.out.println("*******Get by index");
+        System.out.println(dll.get(4).value);
+
 
         System.out.println("*********Remove Last****");
         System.out.println("removed node's value="+dll.removeLast().value);
